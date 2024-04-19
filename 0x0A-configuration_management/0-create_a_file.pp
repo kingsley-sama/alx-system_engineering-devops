481b325e16f74b6a404ab1f28dbf784da7d9c99c
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-19 17:31:33 +0100

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-file {'/temp/school':
-    ensure  => 'file',
-    content => 'I love Puppet',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0774',
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
