96032289f8c6b38f8c451bfba26c07f902a4eac9
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-19 18:36:08 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
 # installs Flask version 2.1.0
-package { 'Flask':
-  ensure   => '2.1.0',
+class { 'python::pip': }
+package { 'flask':
+  ensure   => 'installed',
   provider => 'pip3',
+  require  => Class['python::pip'],
 }
