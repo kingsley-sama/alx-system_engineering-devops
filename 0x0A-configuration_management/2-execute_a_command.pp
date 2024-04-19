0460c40b4e64d2fc41dd695fc38c9cf3b596f844
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-19 18:08:22 +0100

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Puppet manifest to kill a process named killmenow
 exec { 'kill_killmenow_process':
-  command     => '/usr/bin/pkill killmenow',
+  command     => '/usr/bin/pkill -f killmenow',
   refreshonly => true,
 }
