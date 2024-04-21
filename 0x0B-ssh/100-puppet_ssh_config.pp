b713cc535c9545c3e17a28ef0fde158cb9d1e3b4
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-21 22:39:19 +0100

    changes to 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,23 +1,12 @@
-# Removes ssh password and sets up ssh authentication
-file { '/tmp/school.sh':
-  ensure  => file,
-  mode    => '0777',
-  owner   => 'ubuntu',
-  content => @(END),
-    #!/usr/bin/env bash
-    # Generates an new key tha
-    ssh-keygen -t rsa -b 4096 -f ~/.ssh/school
-    END
-}
 # Puppet script to create ssh config file
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
-exec { 'generate_rsa_key':
-  command => '/bin/bash /tmp/school.sh',
-  path    => '/usr/bin:/bin',
-  user    => 'ubuntu',
-}
 
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
