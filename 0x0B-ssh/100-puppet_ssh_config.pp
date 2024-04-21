a848485eea4cc7e1412f17ae65ea37ea30c2d4dd
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-21 21:07:48 +0100

    script to set up remote rsa key

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+# Removes ssh password and sets up ssh authentication
+file { '/tmp/school.sh':
+  ensure  => file,
+  mode    => '0777',
+  owner   => 'ubuntu',
+  content => @(END),
+    #!/usr/bin/env bash
+    # Generates an new key that uses rsa encoding and is stored in scool
+    ssh-keygen -t rsa -b 4096 -f ~/.ssh/school
+    sudo sed -i 's/^.*PasswordAuthentication.*$/PasswordAuthentication no/' /etc/ssh/ssh_config
+    END
+}
+exec { 'generate_rsa_key':
+  command => '/bin/bash /tmp/school.sh',
+  path    => '/usr/bin:/bin',
+  user    => 'ubuntu'
+}
+
