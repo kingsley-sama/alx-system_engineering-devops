22906242aed78fc4f742d07af8df8b2c5578cb18
Kingsley Okpo <kngsley2018@gmail.com>
2024-06-05 16:02:39 +0100

    0-strace_is_your_friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,24 @@
-class { 'apache':
-  mpm_module => 'prefork',
+# Update apt package list
+exec { 'update_apt':
+  command => '/usr/bin/apt-get update',
+  path    => '/usr/bin',
 }
 
-class { 'apache::mod::php': }
+# Install Apache2 package
+package { 'apache2':
+  ensure  => installed,
+  require => Exec['update_apt'],
+}
 
+# Install PHP and related modules
 package { 'php':
   ensure  => installed,
-  require => Class['apache'],
+  require => Exec['update_apt'],
+}
+
+package { 'libapache2-mod-php':
+  ensure  => installed,
+  require => Package['php'],
 }
 
 package { 'php-mysql':
@@ -14,19 +26,16 @@ package { 'php-mysql':
   require => Package['php'],
 }
 
+# Ensure Apache2 service is running and enabled
 service { 'apache2':
   ensure    => running,
   enable    => true,
   subscribe => Package['php'],
 }
 
-exec { 'update_apt':
-  command => '/usr/bin/apt-get update',
-  path    => '/usr/bin',
-  before  => Package['apache2'],
-}
-
-package { 'apache2':
-  ensure  => installed,
-  require => Exec['update_apt'],
+# Restart Apache2 service to apply changes
+exec { 'restart_apache2':
+  command     => '/usr/sbin/service apache2 restart',
+  refreshonly => true,
+  subscribe   => Package['libapache2-mod-php'],
 }
