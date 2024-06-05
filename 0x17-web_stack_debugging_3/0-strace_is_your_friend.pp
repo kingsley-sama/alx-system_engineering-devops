dea6c3bded3b700b3843f4d27e3e5ce1f2c1d7cf
Kingsley Okpo <kngsley2018@gmail.com>
2024-06-05 15:53:34 +0100

    0-strace_is_your_friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,32 @@
+class { 'apache':
+  mpm_module => 'prefork',
+}
+
+class { 'apache::mod::php': }
+
+package { 'php':
+  ensure => installed,
+  require => Class['apache'],
+}
+
+package { 'php-mysql':
+  ensure => installed,
+  require => Package['php'],
+}
+
+service { 'apache2':
+  ensure     => running,
+  enable     => true,
+  subscribe  => Package['php'],
+}
+
+exec { 'update_apt':
+  command => '/usr/bin/apt-get update',
+  path    => '/usr/bin',
+  before  => Package['apache2'],
+}
+
+package { 'apache2':
+  ensure  => installed,
+  require => Exec['update_apt'],
+}
